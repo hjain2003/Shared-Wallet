5b6ba35048f6ceea22acb4966d588c17fd665bcb
Harsh <hjain_be21@thapar.edu>
2024-04-15 17:03:05 +0530

    added ui for seeing requests

--- client/src/MainSection/WalletCard/SharedWallet/SharedWallet.jsx
+++ client/src/MainSection/WalletCard/SharedWallet/SharedWallet.jsx
@@ -1,4 +1,4 @@
-import React,{useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 import swexit from "./exit-icon.svg";
@@ -10,49 +10,49 @@ import vector4 from "./vector4.svg";
 import vector5 from "./vector5.svg";
 
 import "./SharedWallet.css";
-import SettingsSW from './SettingsSW/SettingsSW';
+import SettingsSW from "./SettingsSW/SettingsSW";
 import { SharedContext } from "../../../context/SharedContext";
 
 const pro1 = {
   hidden: {
-    y: '100px',
+    y: "100px",
     opacity: 0,
   },
   visible: {
-    y: '200px',
+    y: "200px",
     opacity: 1,
     transition: { delay: 0.0 },
   },
 };
 
-const SharedWallet = ({ open, onClose, walletId, walletName, goalAmount, borrowLimit, walletBalance}) => {
-
+const SharedWallet = ({ open, onClose, walletId, walletName, goalAmount, borrowLimit, walletBalance }) => {
   const [openSCard, setOpenSCard] = useState(false);
-  const { getNumberOfParticipants, getParticipantsWithAddresses} = useContext(SharedContext);
+  const { getNumberOfParticipants, getParticipantsWithAddresses } = useContext(SharedContext);
   const [numberOfParticipants, setNumberOfParticipants] = useState(0);
   const [isopenparticipants, setopenparticipants] = useState(false);
   const [participantsData, setParticipantsData] = useState([]);
+  const [seeRequests, setRequests] = useState([]);
+  const [isRequestBoxOpen, setRequestBox] = useState(false);
 
-  // const participantsData = [
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   { username: "User1", address: "0x1234...5678" },
-  //   { username: "User2", address: "0xABCD...EFGH" },
-  //   // Add more participant objects as needed
-  // ];
-
+  const requestsData = [
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    { username: "User1", address: "0x1234...5678" },
+    { username: "User2", address: "0xABCD...EFGH" },
+    // Add more participant objects as needed
+  ];
 
   useEffect(() => {
     const fetchNumberOfParticipants = async () => {
@@ -71,26 +71,23 @@ const SharedWallet = ({ open, onClose, walletId, walletName, goalAmount, borrowL
     }
   }, [open, getNumberOfParticipants, walletId]);
 
-
   const convertWeiToEther = (weiValue) => {
     const etherValue = weiValue / 1e18;
     return etherValue.toFixed(2);
   };
 
-  const decimalGoal = convertWeiToEther(goalAmount)/1e18;
-  const decimalbl = convertWeiToEther(borrowLimit)/1e18;
-
+  const decimalGoal = convertWeiToEther(goalAmount) / 1e18;
+  const decimalbl = convertWeiToEther(borrowLimit) / 1e18;
 
   const handleOpenSLogoClick = () => {
     setOpenSCard(true);
-    
   };
 
   function CardSClose() {
     setOpenSCard(false);
   }
 
-  const openParticipants = async()=>{
+  const openParticipants = async () => {
     setopenparticipants(true);
     try {
       const participants = await getParticipantsWithAddresses(walletId);
@@ -98,110 +95,146 @@ const SharedWallet = ({ open, onClose, walletId, walletName, goalAmount, borrowL
     } catch (error) {
       console.error("Error fetching participants:", error);
     }
-  }
+  };
 
-  const closeParticipants = ()=>{
+  const closeParticipants = () => {
     setopenparticipants(false);
     console.log("closed");
-  }
+  };
+
+  const setRequestBoxOpen = () => {
+    setRequestBox(true);
+  };
+
+  const setRequestBoxClose = () => {
+    setRequestBox(false);
+  };
 
   var unit = "ETHEREUM";
   if (!open) return null;
-  
+
   return (
     <>
-   {isopenparticipants &&  (<div className="participant_list">
-    <button id="close_participants" onClick={closeParticipants}>Close</button>
-    <h3>Participants of {walletName}</h3>
-    <input type="text" placeholder="Search Participants" />
-    <br />
-    {participantsData.map((participant, index) => (
+      {/* PARTICIPANTS */}
+      {isopenparticipants && (
+        <div className="participant_list">
+          <button id="close_participants" onClick={closeParticipants}>
+            Close
+          </button>
+          <h3>Participants of {walletName}</h3>
+          <input type="text" placeholder="Search Participants" />
+          <br />
+          {participantsData.map((participant, index) => (
             <div key={index} className="participant-row">
               <span className="username">{participant.username}</span>
-              <span className="address">{`${participant.address.substring(0, 8)}...${participant.address.substring(participant.address.length - 4)}`}</span>
+              <span className="address">{`${participant.address.substring(0, 8)}...${participant.address.substring(
+                participant.address.length - 4
+              )}`}</span>
             </div>
           ))}
-    </div>
-   )}
-    <AnimatePresence>
-    <div>
-      <motion.div
-        class="pro1"
-        id="pro1"
-        variants={pro1}
-        initial="hidden"
-        animate="visible"
-      >
-        <div className="swmain">
-          <div className="swheader">
-            <div className="swh1">{walletName}</div>
-            <div className="swh2">{walletId}</div>
-            <div className="setting-exit-btns">
-              <div className="logo1" onClick={onClose}>
-                <img src={swexit} alt="Exit shared wallet" />
-              </div>
-              <div className="logo2" onClick={handleOpenSLogoClick}>
-                <img src={swsettings} alt="Open shared wallet settings" />
-              </div>
-            </div>
-          </div>
-          <div></div>
-
-          <div className="swinfos">
-          {/* card1 */}
-            <div className="swinfo info1">
-              <div className="swlogo-back">
-                <img src={vector1} />
-              </div>
-              <div className="swvalue">{walletBalance} {unit}</div>
-              <div className="swlogo">Balance</div>
-            </div>
-            {/* card2 */}
-            <div className="swinfo info1">
-              <div className="swlogo-back">
-                <img src={vector2} />
-              </div>
-              <div className="swvalue">{decimalGoal} {unit}</div>
-              <div className="swlogo">Goal</div>
-            </div>
-            {/* card3 */}
-            <div className="swinfo info1">
-              <div className="swlogo-back">
-                <img src={vector3} />
-              </div>
-              <div className="swvalue">{decimalbl} {unit}</div>
-              <div className="swlogo">Borrow Limit</div>
-            </div>
-            <div className="swibottom">
-            {/* card4 */}
-            <div className="swinfo info1 b1 members-card" onClick={openParticipants}>
-              <div className="swlogo-back">
-                <img src={vector4} />
-              </div>
-              <div className="swvalue">{numberOfParticipants}</div>
-              <div className="swlogo">Participants</div>
-            </div>
-            {/* card5 */}
-            <div className="swinfo info1 b2 trn-hist">
-              <div className="swlogo-back">
-                <img src={vector5} />
-              </div>
-              <div className="swvalue">Transaction</div>
-              <div className="swlogo">History</div>
-            </div>
-
-            </div>
-          </div>
-          <div className="swbtn-btm">
-            <button className="leave-wallet" >Request To Join</button>
-          </div>
         </div>
-        <div >
-        {openSCard && <SettingsSW sopen={openSCard} onClose={CardSClose} />}
+      )}
+
+      {/* REQUESTS */}
+      {isRequestBoxOpen && (
+        <div className="participant_list" id="part_requests">
+          <button id="close_participants" onClick={setRequestBoxClose}>
+            Close
+          </button>
+          <h3>Requests for {walletName}</h3>
+          <input type="text" placeholder="Search Requests" />
+          <br />
+          {requestsData.map((participant, index) => (
+            <div key={index} className="participant-row">
+              <span className="username">{participant.username}</span>
+              <span className="address">{`${participant.address.substring(0, 8)}...${participant.address.substring(
+                participant.address.length - 4
+              )}`}</span>
+              <button id="accept">Accept</button>
+              <button id="reject">Reject</button>
+            </div>
+          ))}
         </div>
-      </motion.div>
-      </div>
-    </AnimatePresence>
+      )}
+      <AnimatePresence>
+        <div>
+          <motion.div class="pro1" id="pro1" variants={pro1} initial="hidden" animate="visible">
+            <div className="swmain">
+              <div className="swheader">
+                <div className="swh1">{walletName}</div>
+                <div className="swh2">{walletId}</div>
+                <div className="setting-exit-btns">
+                  <div className="logo1" onClick={onClose}>
+                    <img src={swexit} alt="Exit shared wallet" />
+                  </div>
+                  <div className="logo2" onClick={handleOpenSLogoClick}>
+                    <img src={swsettings} alt="Open shared wallet settings" />
+                  </div>
+                </div>
+              </div>
+              <div></div>
+
+              <div className="swinfos">
+                {/* card1 */}
+                <div className="swinfo info1">
+                  <div className="swlogo-back">
+                    <img src={vector1} />
+                  </div>
+                  <div className="swvalue">
+                    {walletBalance} {unit}
+                  </div>
+                  <div className="swlogo">Balance</div>
+                </div>
+                {/* card2 */}
+                <div className="swinfo info1">
+                  <div className="swlogo-back">
+                    <img src={vector2} />
+                  </div>
+                  <div className="swvalue">
+                    {decimalGoal} {unit}
+                  </div>
+                  <div className="swlogo">Goal</div>
+                </div>
+                {/* card3 */}
+                <div className="swinfo info1">
+                  <div className="swlogo-back">
+                    <img src={vector3} />
+                  </div>
+                  <div className="swvalue">
+                    {decimalbl} {unit}
+                  </div>
+                  <div className="swlogo">Borrow Limit</div>
+                </div>
+                <div className="swibottom">
+                  {/* card4 */}
+                  <div className="swinfo info1 b1 members-card" onClick={openParticipants}>
+                    <div className="swlogo-back">
+                      <img src={vector4} />
+                    </div>
+                    <div className="swvalue">{numberOfParticipants}</div>
+                    <div className="swlogo">Participants</div>
+                  </div>
+                  {/* card5 */}
+                  <div className="swinfo info1 b2 trn-hist">
+                    <div className="swlogo-back">
+                      <img src={vector5} />
+                    </div>
+                    <div className="swvalue">Transaction</div>
+                    <div className="swlogo">History</div>
+                  </div>
+                </div>
+              </div>
+              <div className="swbtn-btm">
+                <button className="leave-wallet">Request To Join</button>
+                <button className="leave-wallet" onClick={setRequestBoxOpen}>
+                  See Requests
+                </button>
+              </div>
+            </div>
+            <div>{openSCard && <SettingsSW sopen={openSCard} onClose={CardSClose} />}</div>
+          </motion.div>
+        </div>
+      </AnimatePresence>
     </>
   );
 };
