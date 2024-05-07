d0c8731cf48b89ed5acfc7c47f2729e150f1de2c
Harsh <hjain_be21@thapar.edu>
2024-05-07 13:47:28 +0530

    redeployed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Shared.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = "0xd90061c4D40c58D81FA018b9b93D4eece18fD0eb";
+export const contractAddress = "0xbc37e3b11601544a14cDE1a6438eb725Bb10f4dA";
