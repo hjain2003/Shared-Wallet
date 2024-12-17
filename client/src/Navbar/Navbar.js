9c23eb359418fdc2566008dc036da966076ea879
Harsh <hjain_be21@thapar.edu>
2024-12-17 23:51:12 +0530

    navbar active

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
-import {useState} from "react";
 
 import etherMate from "./Logo.svg";
 import dashboardLogo from "./dasboard.svg";
@@ -10,62 +9,80 @@ import depositsLogo from "./deposits.svg";
 import profileLogo from "./user.svg";
 import Charity from "../Charity/Charity";
 
-const Navbar = ({name}) => {
-  const [openCCard, setOpenCCard] = useState(false);
+const Navbar = () => {
+  const location = useLocation();
+  const [selectedItem, setSelectedItem] = useState(location.pathname);
 
-  const handleOpenCLogoClick = () => {
-    
-    setOpenCCard(true);
-    
+  const handleItemClick = (path) => {
+    setSelectedItem(path);
   };
 
-  function CardCClose() {
-    setOpenCCard(false);
-  }
+  const handleCharityClick = () => {
+    setSelectedItem("charity");
+  };
 
   return (
     <div className="navbar">
       <div className="emlogo">
         <img src={etherMate} alt="Logo" />
       </div>
-      
-      {/* <Link to="/dashboard" className="linkremove"> */}
-        {/* <div className="nav-element"> */}
-          {/* <img src={dashboardLogo} className="nav-logo" alt="Dashboard" /> */}
-          {/* <div className="nav-topic" id="nav_name">{name}</div> */}
-        {/* </div> */}
-      {/* </Link> */}
 
-      <Link to="/dashboard" className="linkremove">
-        <div className="nav-element">
+      <Link
+        to="/dashboard"
+        className="linkremove"
+        onClick={() => handleItemClick("/dashboard")}
+      >
+        <div
+          className={`nav-element ${
+            selectedItem === "/dashboard" ? "active" : ""
+          }`}
+        >
           <img src={dashboardLogo} className="nav-logo" alt="Dashboard" />
           <div className="nav-topic">Dashboard</div>
         </div>
       </Link>
 
-      <Link to="/chatbot" className="linkremove">
-        <div className="nav-element">
+      <Link
+        to="/chatbot"
+        className="linkremove"
+        onClick={() => handleItemClick("/chatbot")}
+      >
+        <div
+          className={`nav-element ${
+            selectedItem === "/chatbot" ? "active" : ""
+          }`}
+        >
           <img src={chatbotLogo} className="nav-logo" alt="Chatbot" />
           <div className="nav-topic">Chatbot</div>
         </div>
       </Link>
 
-      <Link to="/dashboard" className="linkremove">
-      <div to="/" className="linkremove" onClick={handleOpenCLogoClick}>
-        <div className="nav-element">
-          <img src={depositsLogo} className="nav-logo" alt="Deposits" />
-          <div className="nav-topic">Charity</div>
-        </div>
+      <div
+        className={`nav-element ${
+          selectedItem === "charity" ? "active" : ""
+        }`}
+        onClick={handleCharityClick}
+      >
+        <img src={depositsLogo} className="nav-logo" alt="Deposits" />
+        <div className="nav-topic">Charity</div>
       </div>
-      </Link>
 
-      <Link to="/split" className="linkremove">
-        <div className="nav-element">
+      <Link
+        to="/split"
+        className="linkremove"
+        onClick={() => handleItemClick("/split")}
+      >
+        <div
+          className={`nav-element ${
+            selectedItem === "/split" ? "active" : ""
+          }`}
+        >
           <img src={profileLogo} className="nav-logo" alt="Profile" />
           <div className="nav-topic">Split</div>
         </div>
       </Link>
-      {openCCard && <Charity sopen={openCCard} onClose={CardCClose} />}
+
+      {selectedItem === "charity" && <Charity sopen={true} onClose={() => setSelectedItem("")} />}
     </div>
   );
 };
